92612d4607ee2a997b8b49150dd5deb76e4afbac
Pedro Nogueira <jpedronogueira.dev@gmail.com>
2024-12-13 22:28:00 -0400

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,18 @@
 import Header from "../_components/header";
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="py-6 px-5 gap-1" id="hello">
+        <h2 className="text-xl font-bold">Olá, João</h2>
+        <p className="first-letter:capitalize text-sm">
+          {format(new Date(), "EEE', 'd 'de' MMMM", {
+            locale: ptBR
+          })}</p>
+      </div>
     </div>
   );
 }
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,7 +5,7 @@ import { MenuIcon } from "lucide-react";
 
 const Header = () => {
     return (
-        <Card>
+        <Card className=" border-l-0 border-r-0">
             <CardContent className="px-5 py-4 justify-between flex flex-row">
                 <Image src="/logo.svg" alt="Logo da barbearia" width={130} height={22}/>
                 <Button variant="outline" size="icon">
